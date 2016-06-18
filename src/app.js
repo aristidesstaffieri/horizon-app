f8651ce5233725881265b5747ac096c8dc674d3e
Aristides Staffieri <aristides@aristidesstaffieri.com>
2016-06-18 16:13:19 -0600

    updated sd npm script to serve using https in order to auth locally

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 
 class App extends Component {
+  componentWillMount() {
+    const horizon = Horizon({ authType: 'token' })
+    if (!horizon.hasAuthToken()) {
+      horizon.authEndpoint('github').subscribe((endpoint) => {
+          window.location.pathname = endpoint
+          console.log(endpoint)
+        })
+    } else {
+      console.log('authed!')
+    }
+  }
+
   render() {
     return (
       <div>
@@ -11,4 +24,31 @@ class App extends Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+class Home extends Component {
+  // componentWillMount() {
+  //   const horizon = Horizon({ authType: 'token' })
+  //   if (!horizon.hasAuthToken()) {
+  //     horizon.authEndpoint('github').subscribe((endpoint) => {
+  //         // window.location.pathname = endpoint
+  //         console.log(endpoint)
+  //       })
+  //   } else {
+  //     console.log('authed!')
+  //   }
+  // }
+
+  render() {
+    return (
+      <div>
+        Home!
+      </div>
+    )
+  }
+}
+
+render((
+  <Router history={ hashHistory }>
+    <Route path="/" component={ App }/>
+    <Route path="/home" component={ Home }/>
+  </Router>
+), document.getElementById('root'))
